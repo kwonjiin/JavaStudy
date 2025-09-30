f7b567a1d34852d6d001ae6e115f703a9e6e5c93
DESKTOP-KKJ0FNN\jimin <jimin001006@naver.com>
2025-09-30 15:24:37 +0900

    continue를 사용한 for문

---
+++ chap04/src/sec02/exam10/ContinueExample.java
@@ -0,0 +1,12 @@
+package chap04.src.sec02.exam10;
+
+public class ContinueExample {
+    public static void main(String[] args) throws Exception {
+        for(int i=1; i<=10; i++) {
+            if(i%2 != 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
