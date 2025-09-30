ff3f576eefa7eaa00f7dcdeea3145a9001b1662c
DESKTOP-KKJ0FNN\jimin <jimin001006@naver.com>
2025-09-30 15:08:17 +0900

    breack 문이 없는 case

---
+++ chap04/src/sec01/exam06/SwitchNoBreakCaseExample.java
@@ -0,0 +1,20 @@
+package chap04.src.sec01.exam06;
+
+public class SwitchNoBreakCaseExample {
+    public static void main(String[] args) {
+        //8<= ... <= 11 사이의 정수 뽑기
+        int time = (int)(Math.random()*4) + 8;
+        System.out.println("[현재 시간 : " + time + " 시]");
+
+        switch(time) {
+            case 8:
+                System.out.println("출근합니다.");
+            case 9:
+                System.out.println("회의를 합니다. ");
+            case 10:
+                System.out.println("업무를 봅니다.");
+            default:
+                System.out.println("외근을 나갑니다.");
+        }
+    }
+}
