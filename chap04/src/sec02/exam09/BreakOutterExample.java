0985e870d00761ef2a3e65cb75a519c0abceafda
DESKTOP-KKJ0FNN\jimin <jimin001006@naver.com>
2025-09-30 15:23:49 +0900

    바깥쪽 반복문 종료

---
+++ chap04/src/sec02/exam09/BreakOutterExample.java
@@ -0,0 +1,15 @@
+package chap04.src.sec02.exam09;
+
+public class BreakOutterExample {
+    public static void main(String[] args) throws Exception {
+        Outter: for(char upper='A'; upper<='Z'; upper++) {
+            for(char lower='a'; lower<='z'; lower++) {
+                System.out.println(upper + "-" + lower);
+                if(lower=='g') {
+                    break Outter;
+                }
+            }
+        }
+        System.out.println("프로그램 실행 종료");
+    }
+}
